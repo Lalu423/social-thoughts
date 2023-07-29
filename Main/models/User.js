cb66850402cf4f631a74b4a74ca01462d5886591
Lalu423 <>
2023-07-28 20:19:52 -0400

    created file structure and created user model

---
+++ Main/models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // validate
+    },
+    thoughts: [
+        { 
+            type: Schema.Types.ObjectId,
+            ref: "thought"
+        }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
